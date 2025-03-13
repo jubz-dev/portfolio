aa84242b4b57aa9e1478f8d9118a8d304eb1b606
jubz-dev <salamidarjm@gmail.com>
2025-03-13 22:31:47 +0800

    chore: add HTML boilerplate

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>PORTFOLIO</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" type="image/x-icon" href="./src/icons/logo.png">
+</head>
+<body>
+</body>
+</html>
