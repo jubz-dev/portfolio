46fc08c33736b25ff97c0ee3cfe06d37bfb64e98
jubz-dev <salamidarjm@gmail.com>
2025-03-13 23:00:52 +0800

    feat: add banner/home section with CTA

--- index.php
+++ index.php
@@ -43,6 +43,36 @@
     </nav>
 </header>
 
+<!-- banner section -->
+<section class="max-w-7xl mx-auto px-5" id="home">
+    <div class="flex md:flex-row flex-col justify-between items-center text-white gap-4 py-10">
+      <div class="md:w-1/2">
+          <p class="text-xl font-medium mb-4">
+            Hello, <span class="text-primary">I'm</span>
+          </p>
+          <h1 class="font-bold text-4xl tracking-[3.22px] mb-5">Jubail Salamida</h1>
+          <p class="text-2xl mb-5">
+            Back-End Software Engineer
+          </p>
+          <p class="text-lg mb-9 md:w-3/4 text-justify leading-8">
+            I am passionate about creating impactful web solutions using PHP, web development, and databases, building scalable systems that enhance user experience.
+          </p>
+          <a href="#message"><button class="flex items-center justify-center px-10 py-4 border border-transparent font-medium rounded-md text-secondary bg-green-250 hover:bg-primary cursor-pointer"><i class="bx bxs-chevrons-down text-3xl text-secondar pr-1"></i>Let's Talk</button></a>
+          <div class="mt-9 mb-8 flex gap-4 items-center">
+            <div class="flex space-x-3">
+                <a href="https://www.linkedin.com/in/rudolfo-salamida-5835101a1/" target="_blank"><img src="src/icons/linkedIn.png" alt="" class="w-8 h-8"/></a>
+                <a href="tel:+639953389390"><img src="src/icons/mobile.png" alt="" class="w-8 h-8"/></a>
+                <a href="mailto:rjmsalamida@gmail.com"><img src="src/icons/email.png" alt="" class="w-8 h-8"/></a>
+            </div>
+          </div>
+      </div>
+      <div class="md:w-1/2 order-first md:order-none">
+          <img src="src/img/profilepic.png" alt="" class="md:ml-20 w-full" />
+      </div>
+    </div>
+</section>
+
+
 <!-- script tags -->
 <script src="src/app.js"></script>
 </body>
